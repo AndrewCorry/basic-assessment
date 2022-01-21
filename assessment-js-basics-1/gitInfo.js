aa1be2d9beec4fd290989d64285bca742c485a8f
AndrewCorry <97917285+AndrewCorry@users.noreply.github.com>
2022-01-21 13:08:38 -0600

    Add files via upload

---
+++ assessment-js-basics-1/gitInfo.js
@@ -0,0 +1,77 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+let gitDefinition = 'Git is a software that can backup iterations of code.'
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+let gitHubDefinition = 'GitHub is a website that you can use to access code backed up by Git. It is useful for colaborating with different developers on the same code'
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+gitInitDefinition = 'The git init command initializes a git repository in the current folder.'
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+gitCloneDefinition = 'The git clone command makes a copy of a repository.'
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+gitStatusDefinition = 'The git status command shows which files have or have not been updated in a repository'
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+gitAddDefinition = 'The command git add adds an existing file to a repository'
+
+gitAddCode = 'git add -A'
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+gitCommitDefinition = 'the git commit command commits a version of code to the repository'
+gitCommitCode = 'git commit -m "Initial commit"'
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+gitPushDefinition = 'The git push command uploads a local repository to a remote one'
